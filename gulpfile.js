8bc909a6a6414dfd02921318fef029c453032d85
Shinichi Kogai <kogai0121@gmail.com>
2015-08-26 22:43:41 +0900

    開発環境構築

---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var gulp = require('gulp');
+var newer = require('gulp-newer');
+var objectAssign = require('object-assign');
+
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sass = require('gulp-ruby-sass');
+
+var pngmin = require('gulp-pngmin');
+
+var config = {
+	env: 'development',
+	src: {
+		client: {
+			root: './src'
+,			images: './src/images'
+,			views: './src/views'
+,			sass: './src/sass'
+		},
+		server: './server/src'
+	},
+	dest: {
+		root: './public',
+		images: './public/images'
+	}
+};
+
+gulp.task('sass', function(){
+	return sass(config.src.client.sass + '/style.sass', {
+		compass: true
+,		style: 'compressed'
+,		sourcemap: true
+	})
+	.on('error', function(err){
+		console.log(err);
+	})
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('browserify', function(){
+	var opt = objectAssign(
+		{},
+		{
+			entries: [
+				config.src.client.views + '/App.jsx'
+			],
+			extensions: ['.js'],
+			debug: true
+		}
+	);
+
+	var b;
+
+	b = browserify(opt);
+	return b.transform('babelify')
+	.transform('reactify')
+	.bundle()
+	.pipe(source('bundle.js'))
+	.pipe(buffer())
+	.pipe(sourcemaps.init({
+		loadMaps: true
+	}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('copy-client', function(){
+	return gulp.src([
+		config.src.client.root + '/index.html'
+	])
+	.pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('copy', [
+	'copy-client'
+]);
+
+gulp.task('pngmin', function () {
+	return (
+		gulp.src([
+			config.src.client.images + '/*.png',
+			config.src.client.images + '/**/*.png',
+			config.src.client.images + '/**/**/*.png'
+		])
+		.pipe(newer(config.dest.images))
+		.pipe(pngmin())
+		.pipe(gulp.dest(config.dest.images))
+	);
+});
+
+gulp.task('isProduction', function(){
+	config.env = 'production';
+});
+
+gulp.task('compile', [
+	'copy'
+,	'pngmin'
+,	'browserify'
+,	'sass'
+]);
+
+gulp.task('default', [
+	'compile'
+], function(){
+	gulp.watch([
+		config.src.client.views + '/*.jsx',
+		config.src.client.views + '/**/*.jsx',
+		config.src.client.views + '/**/**/*.jsx',
+		config.src.client.views + '/*.js',
+		config.src.client.views + '/**/*.js',
+		config.src.client.views + '/**/**/*.js'
+	], ['browserify']);
+
+	gulp.watch([
+		config.src.client.images + '/*',
+		config.src.client.images + '/**/*',
+		config.src.client.images + '/**/**/*'
+	], ['pngmin']);
+
+	gulp.watch([
+		config.src.client.sass + '/*',
+		config.src.client.sass + '/**/*',
+		config.src.client.sass + '/**/**/*'
+	], ['sass']);
+});
+
+gulp.task('build', [
+	'isProduction',
+	'compile'
+]);
---
+++ src/views/App.jsx
@@ -0,0 +1,35 @@
+'use babel';
+
+import React from 'react';
+import Router from 'react-router';
+import { Route, HashLocation } from 'react-router';
+
+/*
+import Handler from './Handler.jsx';
+import Index from './pages/Index.jsx';
+import Account from './pages/Account.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Edit from './pages/Edit.jsx';
+import Device from './pages/Device.jsx';
+
+const routes = (
+	<Route handler={ Handler }>
+		<Route name='account' path='/account' handler={ Account } />
+		<Route name='account-register' path='/account/register' handler={ Register } />
+		<Route name='account-login' path='/account/login' handler={ Login } />
+		<Route name='account-edit' path='/account/edit' handler={ Edit } />
+		<Route name='device' path='/device' handler={ Device } />
+		<Router.DefaultRoute handler={ Index } />
+	</Route>
+);
+
+var router = Router.create({
+	routes: routes,
+	location: HashLocation
+});
+
+router.run(function(Root, state){
+	React.render(<Root params={ state.params } />, document.body);
+});
+*/
