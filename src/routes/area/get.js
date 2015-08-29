9c252789d24d0360e5b45fc20c4535ac4ffc8c04
Shinichi Kogai <kogai0121@gmail.com>
2015-08-29 21:14:54 +0900

    レスポンスの言語を指定

--- src/routes/area/get.js
+++ src/routes/area/get.js
@@ -5,13 +5,33 @@ import util from 'util';
 export default (req, res) => {
   let latlong = req.query;
 
+  let options = {
+	  key: geocoderKey,
+	  result_type: ['administrative_area_level_1']
+  };
+
+  let japaneseLang = 'ja';
+  if(req.headers['accept-language'].match(japaneseLang)){
+    options.language = japaneseLang;
+  }
+
 	geocoder.reverseGeocode(latlong.latitude, latlong.longitude, function (err, data) {
 		if(err){
       return res.status(500).send(err);
 		}
-    res.status(200).send(data.results);
-	}, {
-	  key: geocoderKey,
-	  result_type: ['administrative_area_level_1']
-	});
+
+    let result;
+    try{
+      result = data.results[0];
+    }catch(cathcedError){
+      return res.status(400).send(cathcedError);
+    }
+
+    res.status(200).send({
+      body: result,
+      formatted_address: result.formatted_address,
+      location: result.geometry.location
+    });
+
+	}, options);
 };
---
+++ test/routes/area/get.js
@@ -0,0 +1,5 @@
+// 函館
+let latlong = {
+  latitude: '41.8596699',
+  longitude: '140.9399629'
+};
